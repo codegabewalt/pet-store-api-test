6ee0d4f45e148968b818af4dda069809e411d69c
Gabriel (Gabe) <104101625+codegabewalt@users.noreply.github.com>
2023-12-27 17:51:51 +0000

    Worked on pet-endpoint.spec

--- tests/pet-endpoint.spec.js
+++ tests/pet-endpoint.spec.js
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 const pet = require("../data/pet.json");
+const petUpdated = require("../data/petUpdated.json");
 
 test.beforeAll("Add new available pet", async ({ request }) => {
   const res = await request.post("pet", {
@@ -15,4 +16,19 @@ test("Check new available pet", async ({ request }) => {
   const json = await res.json();
 
   expect(pet.category.name).toBe(json.category.name);
+});
+
+test("Update new availible pet", async ({ request }) => {
+  const res = await request.put("pet", {
+    data: petUpdated,
+  });
+
+  expect(res.status()).toBe(200);
+});
+
+test("Check updated new available pet", async ({ request }) => {
+  const res = await request.get(`pet/${petUpdated.id}`);
+  const json = await res.json();
+
+  expect(petUpdated.category.name).toBe(json.category.name);
 });
